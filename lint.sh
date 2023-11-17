43885dc782fd1faf26ba11d2794bb6349cb16c81
Théo Tessilimi <tessilimi.theo@gmail.com>
2023-11-17 11:06:52 +0100

    Create lint.sh
    first commit

---
+++ lint.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+echo "Linting Clean Architecture..."
+
+# Utilisation de la commande find pour rechercher tous les fichiers correspondant au modèle */app/use-cases/*.handler.ts
+filesUsecases=$(find src -path "*/app/use-cases/*.ts" -type f | grep -v "/test/" | grep -v "\.spec\.ts$")
+filesApp=$(find src -path "*/app/*.ts" -type f | grep -v "/test/" | grep -v "\.spec\.ts$")
+filesInterface=$(find src -path "*/port/*.ts" -type f | grep -v "/test/" | grep -v "\.spec\.ts$")
+filesEntity=$(find src -path "*/infra/datastore/entity/*.ts" -type f | grep -v "/test/" | grep -v "\.spec\.ts$")
+filesDomain=$(find src -path "*/domain/*.ts" -type f | grep -v "/test/" | grep -v "\.spec\.ts$")
+
+
+fileCounter=0
+
+
+# Vérifier si tous les fichiers se terminent par .handler.ts
+for file in $filesUsecases; do
+  echo "check for file name in: " $file
+  if [[ "$file" != *.handler.ts ]]; then
+    echo "Error: File $file does not end with .handler.ts"
+    exit 1
+  fi
+   ((fileCounter++))
+done
+
+for file in $filesEntity; do
+  echo "check for file name in: " $file
+  if [[ "$file" != *.entity.ts ]]; then
+    echo "Error: File $file does not end with .entity.ts"
+    exit 1
+  fi
+   ((fileCounter++))
+done
+
+for file in $filesDomain; do
+  echo "check for DDD principles in:" $file
+  if grep -q -E "class.*Entity|class.*ValueObject|class.*Aggregate" "$file"; then
+    echo "Error: File $file violates DDD principles in /domain."
+    exit 1
+  fi
+
+  # Incrémenter le compteur de fichiers
+  ((fileCounter++))
+done
+
+# Ajouter des vérifications supplémentaires ici pour les appels de framework dans /domain ou /app
+for file in $filesApp; do
+  echo "check for dependency in:" $file
+  if grep -q "@nestjs" "$file"; then
+    echo "Error: File $file contains a framework call, which is not allowed in /domain or /app."
+    exit 1
+  fi
+   ((fileCounter++))
+done
+
+for file in $filesInterface; do
+  echo "check for interface in:" $file
+  if grep -q -E "class|enum" "$file"; then
+    if ! grep -q "interface" "$file"; then
+      echo "Error: File $file contains a class or enum, but it does not contain an interface, which is not allowed in /domain or /app."
+      exit 1
+    fi
+  fi
+   ((fileCounter++))
+done
+
+echo "Clean Architecture validation passed. check $fileCounter files."
